91616f49c98d644317d6000f3eec3d15f2134630
OwenBryant <owen_bryant@aol.com>
2017-03-10 08:06:00 -0500

    Update module2.sh

--- code/module2.sh
+++ code/module2.sh
@@ -1,1 +1,13 @@
-#
+# An example of how to find all the files in your documents directory.
+ls documents
+
+# This is an exapmle of a comment, becuase I have the # symbol infront of this line, it is not read as a command.
+
+# If I wanted to create a blank file named umbrella I could use touch.
+touch umbrella
+
+# If I made an error or need to get rid of a file or directory, I would use the rm command
+rm umbrella
+
+# Echo just repeats the argument that follows, if I was lonely and wanted my terminal to say hello i would type the following.
+echo hello
