1fe4a8e4976ef0fa994e56e9dfaa75dfa0adc64d
OwenBryant <owen_bryant@aol.com>
2017-03-23 13:53:47 -0400

    Create module3.sh

---
+++ code/module3.sh
@@ -0,0 +1,13 @@
+# WHITESPACE
+"    " inbetween the double quotes is the whitespace
+
+# EXPANSION - $
+$ echo "I am $LOGNAME"
+
+# This is a comment.
+
+# DOUBLE QUOTES -  " "
+echo "That dog is cool"
+
+# REDIRECTION - ><
+$ echo boo > file
